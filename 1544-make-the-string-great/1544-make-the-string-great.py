872db9daede216036f088ed1f6c8634d794bc810
nikitanigam <nikitanigam321@gmail.com>
2026-06-12 20:58:24 +0530

    Time: 3 ms (44.29%), Space: 19.4 MB (18.64%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -0,0 +1,14 @@
+class Solution:
+    def makeGood(self, s: str) -> str:
+        a=s.lower()
+        stack=[]
+        if s=="":
+            return s
+        stack.append(s[0])
+        for i in range(1,len(s)):
+            if len(stack)!=0 and abs(ord(s[i])-ord(stack[-1]))==32:
+                stack.pop()
+            
+            else:
+                stack.append(s[i])
+        return "".join(stack)
